aa40ff2f8293ea962c2182b31213e822ecc3a72d
ralzinov <azimut.nt@gmail.com>
2024-01-29 19:20:37 +0700

    Wip

--- packages/browser/src/background/services/FirebaseAuthService.ts
+++ packages/browser/src/background/services/FirebaseAuthService.ts
@@ -1,29 +1,29 @@
-import { BaseAuthService } from '@lckw/lib-services';
-import type { IConfig } from '~src/background/background.const';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 import type { AuthEvent } from '@lckw/lib-services/dist/src/BaseAuthService';
+import type { IConfig } from '~src/background/background.const';
+import { BaseAuthService } from '@lckw/lib-services';
 
 export class FirebaseAuthService extends BaseAuthService {
-    private _token: string | null = null;
+    private auth = getAuth();
 
     constructor(private config: IConfig) {
         super();
     }
 
     async getIdToken(): Promise<string | null> {
-        return this._token;
+        return this.auth.currentUser?.getIdToken() || null;
     }
 
     async signInAnonymously(): Promise<void> {
-        // sign in firebase
-        // set token
+        await signInAnonymously(this.auth);
     }
 
     async signOut(): Promise<void> {
-        // revoke token;
-        this._token = null;
+        await this.auth.signOut();
     }
 
     addEventListener(fn: (event: AuthEvent) => void): number {
+        // TODO
         return 0;
     }
 }
